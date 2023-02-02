d2cf7f3507f825cd0bf5323f5703a74dae5cf5f9
Paul Slangen <plg.slangen@gmail.com>
2023-02-02 15:46:43 +0100

    Snap er niks van (#13)

--- index.php
+++ index.php
@@ -14,31 +14,56 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 /******************************/
 
 /// Next step, iets met je data doen. Ophalen of zo
-require_once("lib/gerecht.php");
-$gerecht = new gerecht();
-$data = $gerecht->selecteerGerecht();
+require_once("lib/database.php");
+require_once("lib/article.php");
+require_once("lib/user.php");
+require_once("lib/kitchentype.php");
+require_once("lib/ingredient.php");
+require_once("lib/recipe_info.php");
+require_once("lib/recipe.php");
+require_once("lib/groceries.php");
+
+
+$db = new database();
+$article = new article ($db->getConnection());
+$user = new user($db->getConnection());
+$kitchentype = new kitchentype($db->getConnection());
+$ingredient = new ingredient($db->getConnection(), $article);
+$recipeinfo = new recipeinfo($db->getConnection(), $user);
+$recipe = new recipe ($db->getConnection(), $user, $kitchentype, $ingredient, $recipeinfo);
+$groceries = new groceries ($db->getConnection(), $user, $ingredient, $article);
+
+
+$articleData = $article->selectArticle(1);
+$userData = $user->selectUser(1);
+$kitchentypeData = $kitchentype->selectKitchentype(1);
+$ingredientData = $ingredient->selectIngredient(2);
+$recipeinfoData = $recipeinfo->selectInfo(2, 'F');
+$recipeData = $recipe->selectRecipe(0);
+$groceriesData = $groceries->AddGroceries(4,1);
+
 
 
 /*
 URL:
-http://localhost/index.php?gerecht_id=4&action=detail
+http://localhost/index.php?recipe_id=4&action=detail
 */
 
-$gerecht_id = isset($_GET["gerecht_id"]) ? $_GET["gerecht_id"] : "";
+$recipe_id = isset($_GET["recipe_id"]) ? $_GET["recipe_id"] : "";
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 
 
 switch($action) {
 
         case "homepage": {
-            $data = $gerecht->selecteerGerecht();
-            $template = 'detail.html.twig';
+            $data = $recipe->selectRecipe($recipe_id);
+            $template = 'homepage.html.twig';
             $title = "homepage";
             break;
         }
 
         case "detail": {
-            $data = $gerecht->selecteerGerecht($gerecht_id);
+            $data = $recipe->selectRecipe($recipe_id);
             $template = 'detail.html.twig';
             $title = "detail pagina";
             break;
